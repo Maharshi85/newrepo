45b4a6004138b00a63994afabe0b1b7c13c1cf03
Maharshi85 <maharshi.pradhan@gmail.com>
2024-07-28 14:12:32 +0530

    firstcommit

---
+++ slenium.org.in/src/main/java/slenium/org/in/genericutilities/BaseClass.java
@@ -0,0 +1,12 @@
+package slenium.org.in.genericutilities;
+
+import org.testng.annotations.Test;
+
+public class BaseClass {
+
+	@Test
+	public void test()
+	{
+		System.out.println("hello github");
+	}
+}
